e1558acc36f35acbf986f47de6e39e81cca1c5ad
kjthier <karinjthier@gmail.com>
2023-08-31 11:57:29 +0200

    comment absolute imports in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Aliases relative to the parent folder of vite.config.ts
-      frontend: '/frontend',
-      backend: '/backend',
-      types: '/types',
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     // Aliases relative to the parent folder of vite.config.ts
+  //     frontend: '/frontend',
+  //     backend: '/backend',
+  //     types: '/types',
+  //   },
+  // },
 })
