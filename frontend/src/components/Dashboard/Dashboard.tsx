61fe9d61f5dd2ef578a534e7a58359e5b9978657
kjthier <karinjthier@gmail.com>
2023-09-22 17:36:50 +0200

    fixed functionality of packery & draggabilly on dash

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Packery from 'packery'
 import Draggabilly from 'draggabilly'
 import Today from './Today'
@@ -10,64 +10,84 @@ import './Dashboard.css'
 
 const Dashboard: React.FC = () => {
     const dashboardRef = useRef<HTMLDivElement | null>(null)
+    const [layoutOrder, setLayoutOrder] = useState<string[]>([])
 
     useEffect(() => {
-        if (dashboardRef.current) {
+        const savedLayout = localStorage.getItem('dashboardLayout')
+        if (savedLayout) {
+            setLayoutOrder(JSON.parse(savedLayout))
+        } else {
+            setLayoutOrder([
+                'today',
+                'goals',
+                'completedCourses',
+                'notes',
+                'achievements',
+            ])
+        }
+    }, [])
+
+    useEffect(() => {
+        if (dashboardRef.current && layoutOrder.length > 0) {
             const packery = new (Packery as any)(dashboardRef.current, {
                 itemSelector: '.dashboard-item',
                 gutter: 10,
             })
 
-            // Load layout from localStorage
-            const savedLayout = localStorage.getItem('dashboardLayout')
-            if (savedLayout) {
-                const layout = JSON.parse(savedLayout)
-                layout.forEach((id: string) => {
-                    const item = document.querySelector(`[data-id="${id}"]`)
-                    if (item && dashboardRef.current) {
-                        dashboardRef.current.appendChild(item)
-                    }
-                })
-                packery.layout()
-            }
-
-            // Make draggable
-            const draggies: Draggabilly[] = []
-            const items: HTMLElement[] = packery.getItemElements()
-            items.forEach((item: HTMLElement) => {
-                const draggie = new Draggabilly(item)
-                draggies.push(draggie)
-                packery.bindDraggabillyEvents(draggie)
-            })
-
-            // Save layout to localStorage
             packery.on('dragItemPositioned', () => {
-                const layout = items.map(
+                const items: HTMLElement[] = packery.getItemElements()
+                const newLayout = items.map(
                     (item: HTMLElement) =>
                         item.getAttribute('data-id') as string
                 )
-                localStorage.setItem('dashboardLayout', JSON.stringify(layout))
+                setLayoutOrder(newLayout)
+                localStorage.setItem(
+                    'dashboardLayout',
+                    JSON.stringify(newLayout)
+                )
             })
+
+            // Separate this to ensure packery layout is done before making items draggable
+            const items: HTMLElement[] = packery.getItemElements()
+            items.forEach((item: HTMLElement) => {
+                const draggie = new Draggabilly(item)
+                packery.bindDraggabillyEvents(draggie)
+            })
+
+            packery.layout()
         }
-    }, [])
+    }, [layoutOrder])
 
     return (
         <div ref={dashboardRef} className='dashboard'>
-            <div className='dashboard-item' data-id='today'>
-                <Today />
-            </div>
-            <div className='dashboard-item' data-id='goals'>
-                <Goals />
-            </div>
-            <div className='dashboard-item' data-id='completedCourses'>
-                <CompletedCourses />
-            </div>
-            <div className='dashboard-item' data-id='notes'>
-                <Notes />
-            </div>
-            <div className='dashboard-item' data-id='achievements'>
-                <Achievements />
-            </div>
+            {layoutOrder.map((id) => {
+                let component = null
+                switch (id) {
+                    case 'today':
+                        component = <Today />
+                        break
+                    case 'goals':
+                        component = <Goals />
+                        break
+                    case 'completedCourses':
+                        component = <CompletedCourses />
+                        break
+                    case 'notes':
+                        component = <Notes />
+                        break
+                    case 'achievements':
+                        component = <Achievements />
+                        break
+                    default:
+                        component = null
+                }
+
+                return (
+                    <div key={id} className='dashboard-item' data-id={id}>
+                        {component}
+                    </div>
+                )
+            })}
         </div>
     )
 }
